97b45e9c9ab1ccddf291cfe49f17846c213fe7d7
wya002 <dndyddks001@naver.com>
2022-12-12 16:46:17 +0900

    1차 구현 1. 사이드 바 2. 상단바

---
+++ GUI/Menubutton.xaml.cs
@@ -0,0 +1,47 @@
+﻿using MahApps.Metro.IconPacks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GUI
+{
+    /// <summary>
+    /// Menubutton.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class Menubutton : UserControl
+    {
+        public Menubutton()
+        {
+            InitializeComponent();
+        }
+
+        public PackIconMaterialKind Icon
+        {
+            get { return (PackIconMaterialKind)GetValue(IconProperty); }
+            set { SetValue(IconProperty, value); }
+        }
+
+        public static readonly DependencyProperty IconProperty =
+            DependencyProperty.Register("Icon", typeof(PackIconMaterialKind), typeof(Menubutton));
+
+        public bool IsActive
+        {
+            get { return (bool)GetValue(IsActiveProperty); }
+            set { SetValue(IsActiveProperty, value); }
+        }
+
+        public static readonly DependencyProperty IsActiveProperty =
+            DependencyProperty.Register("IsActive", typeof(bool), typeof(Menubutton));
+    }
+}
