9a3f68ed1c379f693913385237ed91791cc670bd
wya002 <dndyddks001@naver.com>
2022-12-13 10:52:44 +0900

    feat : Scenario Scene

--- GUI/MainWindow.xaml.cs
+++ GUI/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿
+using System;
 using System.Windows;
 using System.Windows.Input;
+using System.Windows.Navigation;
 
 namespace GUI
 {
@@ -20,6 +22,27 @@ namespace GUI
             {
                 this.DragMove();
             }
+        }
+
+        private void Menubutton_MouseEnter(object sender, MouseEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Menubutton_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+        //종료 이벤트 버튼 클릭
+        private void Menubutton_Clicked(object sender, MouseButtonEventArgs e)
+        {
+            
+        }
+        //시나리오 페이지 이동
+        private void Menubutton_LostMouseCapture(object sender, MouseEventArgs e)
+        {
+            ScenarioPage scenepage = new ScenarioPage();
+            this.Content = scenepage;
         }
     }
 }
